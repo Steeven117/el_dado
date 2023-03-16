5634f5e2ff1f55f9cdea09d1e1e75d90076b1161
Walter Steeven Navarro Plaza <125583878+Steeven117@users.noreply.github.com>
2023-03-15 21:28:15 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # el_dado
-cara contraria
+un dado tiene 6 caras, dado un numero de 1 a 6, se desea mostrar el número que aparece en el lado opuesto del dado
---
+++ programa.py
@@ -0,0 +1,36 @@
+x = int(input("ingrese el número: "))
+
+if x==1 :
+
+    rta = "La cara contraria es: 6"
+
+else :
+    if x==2 :
+
+        rta = "La cara contraria es: 5"
+
+    else :
+        if x==3 :
+
+            rta = "La cara contraria es: 4"
+
+        else :
+            if x==4 :
+
+                rta = "La cara contraria es: 3"
+
+            else :
+                if x==5 :
+
+                    rta = "La cara contraria es: 2"
+
+                else:
+                    if x==6 :
+
+                        rta = "La cara contraria es: 1"
+
+                    else :
+
+                        rta = "no joda, ese número nisiquiera es del dado"
+
+print (rta)
